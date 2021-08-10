8bb7de20daa871ea61d6c8dfd9bf7859c47a24fd
Yaroslav.Pashkevich <yaroslav3091989@gmail.com>
2021-08-10 22:18:17 +0300

    Initial Commit

---
+++ homework3Zadanie2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework3Zadanie2
+//
+//  Created by  Yaroslav on 10.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
